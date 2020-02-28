a99f729f9f1fd8066bb8f6f342c19147acfee3ca
nicole1ee <ly.nicole@163.com>
2020-02-28 15:59:17 +0800

    1

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import LyBotton from "./components/botton.vue"
+ 
 Vue.config.productionTip = false
+
+
+
+Vue.component(LyBotton.name,LyBotton)
 
 new Vue({
   render: h => h(App)
